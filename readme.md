3e1864d407c9056665991fdb0d6b768347ef036c
Swikriti Neupane <okayswik@gmail.com>
2022-07-12 20:43:19 +0545

    Updated Weather APP with web-server and express

---
+++ readme.md
@@ -0,0 +1,18 @@
+## node-js
+
+- setTimeout() is used to know how asycn is used.
+
+# Call Stack - Callback Queue
+
+- Main function always gets
+
+## HTTP Requests
+
+- core to make real life application
+- to send mail, message anyone over internet.
+
+- fire request, get response
+
+## Weather API
+
+b52a0d7adf4cc3beec9ae17dc16b502f
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,23 @@
-const request = require("request");
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url =
-  "http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596";
+const address = process.argv[2]
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
-});
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
