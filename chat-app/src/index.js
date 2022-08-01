743ddc00b52e94e53c3227b684e8266f5a82a656
Swikriti Neupane <okayswik@gmail.com>
2022-08-01 20:13:24 +0545

    Geolocation sent through the chat application

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,8 +12,31 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+let count = 0;
+
+//server (emit) -> client (recieve) - countUpdated
+//client (emit) -> client (recieve) - increment
+
+io.on("connection", (socket) => {
   console.log("New Websocket Connection");
+
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("sendLocation", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
 });
 
 server.listen(port, () => {
