48119daec6278967baac62b4e45957030ec96be9
Swikriti Neupane <okayswik@gmail.com>
2022-09-02 18:04:16 +0545

    NODE: SETTIMEOUT: EXPRESS

---
+++ web-server/public/js/setimeout.js
@@ -0,0 +1,27 @@
+//Set Timeout with Event Looping
+
+const fs = require("fs");
+
+function someAsyncOperation(callback) {
+  // Assume this takes 95ms to complete
+  fs.readFile("/path/to/file", callback);
+}
+
+const timeoutScheduled = Date.now();
+
+setTimeout(() => {
+  const delay = Date.now() - timeoutScheduled;
+
+  console.log(`${delay}ms have passed since I was scheduled`);
+}, 100);
+
+// do someAsyncOperation which takes 95 ms to complete
+someAsyncOperation(() => {
+  //Async Operation
+  const startCallback = Date.now();
+
+  // do something that will take 10ms...
+  while (Date.now() - startCallback < 10) {
+    // do nothing
+  }
+});
